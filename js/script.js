905f6e03ccfc4dec2f97e472d04f0cea98aaf4c5
RubelShahriar <mr.rubelshahriar@gmail.com>
2021-08-21 19:52:18 +0600

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,95 +1,131 @@
-//A click function for Memory
-document.getElementById('memoryone').addEventListener('click', function(){
-    const memoryCost = document.getElementById('memory-cost').innerText = '0';
+
+
+    const momeryClick = document.getElementById('memoryone').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        console.log(bestPriceInt);
+        const storageCost = document.getElementById('storage-cost').innerText;
+        const storageCostInt = parseInt(storageCost);
+        console.log(storageCostInt)
+        const deliveryCost = document.getElementById('delivery-cost').innerText;
+        const deliveryCostInt = parseInt(deliveryCost);
+        console.log(deliveryCostInt)
+        const memoryCost = document.getElementById('memory-cost').innerText = '0';
     const memoryCostInt = parseInt(memoryCost);
-})
-
-//A click function for Memory
-
-document.getElementById('memorytwo').addEventListener('click', function(){
-    const memoryCost = document.getElementById('memory-cost').innerText = '180';
-    const memoryCostInt = parseInt(memoryCost);
     console.log(memoryCostInt)
-
-    const total = document.getElementById('total-cost').innerText;
-    const totalCost = parseInt(total);
-    console.log(totalCost)
-
-    // final cost calsulate
-     const finalCost = document.getElementById('total-cost').innerText = totalCost + memoryCostInt;
-    document.getElementById('final-amount').innerText = finalCost;
-})
-
-//A click function for Memory
-document.getElementById('storageone').addEventListener('click', function(){
-    const storageCost = document.getElementById('storage-cost').innerText = '0';
-    const memoryCostInt = parseInt(storageCost);
-})
-
-//A click function for Memory
-document.getElementById('storagetwo').addEventListener('click', function(){
-    const storage = document.getElementById('storage-cost').innerText = '100';
-    const memoryCostInt = parseInt(storage);
-    console.log(memoryCostInt)
-
-    const total = document.getElementById('total-cost').innerText;
-    const totalCost = parseInt(total);
-    console.log(totalCost)
-
-    // final cost calsulate
-    const finalCost = document.getElementById('total-cost').innerText = totalCost + memoryCostInt;
-    document.getElementById('final-amount').innerText = finalCost;
-})
-
-//A click function for Memory
-document.getElementById('storagethree').addEventListener('click', function(){
-    const storageCost = document.getElementById('storage-cost').innerText = '180';
-    const memoryCostInt = parseInt(storageCost);
-    console.log(memoryCostInt)
-
-    const total = document.getElementById('total-cost').innerText;
-    const totalCost = parseInt(total);
-    console.log(totalCost)
-
-    // final cost calsulate
-    const finalCost = document.getElementById('total-cost').innerText = totalCost + memoryCostInt;
-    document.getElementById('final-amount').innerText = finalCost;
-
-})
-
-//A click function for Memory
-document.getElementById('deliveryone').addEventListener('click', function(){
-    const deliveryCost = document.getElementById('delivery-cost').innerText = '0';
-    const memoryCostInt = parseInt(deliveryCost);
-})
-
-//A click function for Memory
-document.getElementById('deliverytwo').addEventListener('click', function(){
-    const deliveryCost = document.getElementById('delivery-cost').innerText = '20';
-    const memoryCostInt = parseInt(deliveryCost);
-    console.log(memoryCostInt)
-
-    const total = document.getElementById('total-cost').innerText;
-    const totalCost = parseInt(total);
-    console.log(totalCost)
     
-    // finalCost calculate
-    const finalCost = document.getElementById('total-cost').innerText = totalCost + memoryCostInt;
-    document.getElementById('final-amount').innerText = finalCost;
-})
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
 
-//convert best price to an integer number
-const bestPrice = document.getElementById('best-price').innerText;
-const bestPriceInt = parseInt(bestPrice);
+    const momeryClick1 = document.getElementById('memorytwo').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        console.log(bestPriceInt);
+        const storageCost = document.getElementById('storage-cost').innerText;
+        const storageCostInt = parseInt(storageCost);
+        console.log(storageCostInt)
+        const deliveryCost = document.getElementById('delivery-cost').innerText;
+        const deliveryCostInt = parseInt(deliveryCost);
+        console.log(deliveryCostInt)
+        const memoryCost = document.getElementById('memory-cost').innerText = '180';
+    const memoryCostInt = parseInt(memoryCost);
+    console.log(memoryCostInt)
+    
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
 
-// function for pomocode discount
-function pomocode(){
+
+    const storageClick = document.getElementById('storageone').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        const storageCost = document.getElementById('storage-cost').innerText = '0';
+        const storageCostInt = parseInt(storageCost);
+        const deliveryCost = document.getElementById('delivery-cost').innerText;
+        const deliveryCostInt = parseInt(deliveryCost);
+        const memoryCost = document.getElementById('memory-cost').innerText ;
+    const memoryCostInt = parseInt(memoryCost);
+    
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
+
+    const storageClick2 = document.getElementById('storagetwo').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        const storageCost = document.getElementById('storage-cost').innerText = '100';
+        const storageCostInt = parseInt(storageCost);
+        const deliveryCost = document.getElementById('delivery-cost').innerText;
+        const deliveryCostInt = parseInt(deliveryCost);
+        const memoryCost = document.getElementById('memory-cost').innerText ;
+    const memoryCostInt = parseInt(memoryCost);
+    
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
+
+    const storageClick3 = document.getElementById('storagethree').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        const storageCost = document.getElementById('storage-cost').innerText = '180';
+        const storageCostInt = parseInt(storageCost);
+        const deliveryCost = document.getElementById('delivery-cost').innerText;
+        const deliveryCostInt = parseInt(deliveryCost);
+        const memoryCost = document.getElementById('memory-cost').innerText ;
+    const memoryCostInt = parseInt(memoryCost);
+    
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
+
+
+
+    const deliveryClick = document.getElementById('deliveryone').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        const storageCost = document.getElementById('storage-cost').innerText;
+        const storageCostInt = parseInt(storageCost);
+        const deliveryCost = document.getElementById('delivery-cost').innerText = '0';
+        const deliveryCostInt = parseInt(deliveryCost);
+        const memoryCost = document.getElementById('memory-cost').innerText ;
+    const memoryCostInt = parseInt(memoryCost);
+    
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
+
+
+    const deliveryClick2 = document.getElementById('deliverytwo').addEventListener('click', function(){
+        const bestPrice = document.getElementById('best-price').innerText;
+        const bestPriceInt = parseInt(bestPrice);
+        const storageCost = document.getElementById('storage-cost').innerText;
+        const storageCostInt = parseInt(storageCost);
+        const deliveryCost = document.getElementById('delivery-cost').innerText = '20';
+        const deliveryCostInt = parseInt(deliveryCost);
+        const memoryCost = document.getElementById('memory-cost').innerText ;
+    const memoryCostInt = parseInt(memoryCost);
+    
+        const total = bestPriceInt + memoryCostInt + storageCostInt + deliveryCostInt;
+        const finalCost = document.getElementById('total-cost').innerText = total;
+        document.getElementById('final-amount').innerText = finalCost;
+    })
+
+
+document.getElementById('discount').addEventListener('click', function(){
     const inputText = document.getElementById('steve').value;
     const final = document.getElementById('final-amount').innerText;
     if(inputText == 'stevekaku'){
         const amount = final * 0.2;
         // console.log(amount);
+        document.getElementById('discount').disabled = true;
         const finalMoney = document.getElementById('final-amount').innerText;
         document.getElementById('final-amount').innerText = finalMoney - amount;
     }
-}
+})
